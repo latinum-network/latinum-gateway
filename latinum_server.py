1b2d6d82ac48971236fd975cb4a2cd548e5e781e
latinum-network <info@latinum.network>
2026-05-10 18:22:19 -0500

    Update latinum_server.py

--- latinum_server.py
+++ latinum_server.py
@@ -60,4 +60,4 @@ async def get_stats():
     except: return {"status": "Syncing"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
